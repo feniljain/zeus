ac91ab318d102d491dd614d2c5394df192785516
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-10-18 14:24:08 +0530

    Removing errors in participant impl

--- pkg/participant/impl.go
+++ pkg/participant/impl.go
@@ -23,7 +23,7 @@ func (r *repo) CreateParticipant(req CreateParticipantReq) error {
 
 	collection := r.client.Database("testing").Collection("participants")
 
-	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	res, err := collection.InsertOne(ctx, bson.M{"name": "someone", "email": "someone@gmail.com", "rsvp": "yes"})

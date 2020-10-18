f786fe8e397fca21fe9493565d3d066124738ca0
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-10-18 14:19:37 +0530

    Testing out removal of non detection

--- pkg/meeting/impl.go
+++ pkg/meeting/impl.go
@@ -41,3 +41,39 @@ func (r *repo) CreateMeeting(req CreateMeetingReq) error {
 
 	return nil
 }
+
+//Service ...
+type Service interface {
+	CreateMeeting(CreateMeetingReq) error
+}
+
+type meetingSvc struct {
+	repo Repository
+}
+
+//MakeNewMeetingService ...
+func MakeNewMeetingService(repo Repository) Service {
+	return &meetingSvc{repo: repo}
+}
+
+func (mSvc *meetingSvc) CreateMeeting(req CreateMeetingReq) error {
+	return mSvc.repo.CreateMeeting(req)
+}
+
+//import (
+//	entities "github.com/feniljain/zeus/pkg/entities"
+//)
+
+//Repository ...
+type Repository interface {
+	CreateMeeting(CreateMeetingReq) error
+}
+
+//CreateMeetingReq represents the structure for creating a meeting
+type CreateMeetingReq struct {
+	UID          string   `json:"uid"`
+	Title        string   `json:"name"`
+	StartTime    string   `json:"starttime"`
+	EndTime      string   `json:"endtime"`
+	Participants []string `json:"participants"`
+}

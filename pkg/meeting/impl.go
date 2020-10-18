eab38efed69c7a2076a18c8045a8383763f05f92
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-10-18 14:40:35 +0530

    trying to repopulate service file and structure error of meeting

--- pkg/meeting/impl.go
+++ pkg/meeting/impl.go
@@ -41,39 +41,3 @@ func (r *repo) CreateMeeting(req CreateMeetingReq) error {
 
 	return nil
 }
-
-//Service ...
-type Service interface {
-	CreateMeeting(CreateMeetingReq) error
-}
-
-type meetingSvc struct {
-	repo Repository
-}
-
-//MakeNewMeetingService ...
-func MakeNewMeetingService(repo Repository) Service {
-	return &meetingSvc{repo: repo}
-}
-
-func (mSvc *meetingSvc) CreateMeeting(req CreateMeetingReq) error {
-	return mSvc.repo.CreateMeeting(req)
-}
-
-//import (
-//	entities "github.com/feniljain/zeus/pkg/entities"
-//)
-
-//Repository ...
-type Repository interface {
-	CreateMeeting(CreateMeetingReq) error
-}
-
-//CreateMeetingReq represents the structure for creating a meeting
-type CreateMeetingReq struct {
-	UID          string   `json:"uid"`
-	Title        string   `json:"title"`
-	StartTime    string   `json:"starttime"`
-	EndTime      string   `json:"endtime"`
-	Participants []string `json:"participants"`
-}
